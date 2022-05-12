bfe0c16eba6e56b81ed330a2bd601423e9acd46b
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-12 19:00:41 +0800

    [EDIT] Update time_calculator.py
    - added optional parameter [day=''] to the function
    - created variables to hold hours and minutes of the start and duration
    - converted the variables from string to int.

--- Time Calculator/time_calculator.py
+++ Time Calculator/time_calculator.py
@@ -1,4 +1,18 @@
-def add_time(start, duration):
+def add_time(start, duration, day=''):
+
+    # split the start time and duration into separate variables for easier calculation.
+    # split start time and denomination
+    start_time, start_denomination = start.split()
+    # further split start time into hours and minutes
+    start_time_hour, start_time_minutes = start_time.split(':')
+    # split duration into hour and minutes
+    duration_hour, duration_minutes = duration.split(':')
+
+    # converted all the hours and minutes into integer for calculation
+    duration_hour = int(duration_hour)
+    duration_minutes = int(duration_minutes)
+    start_time_hour = int(start_time_hour)
+    start_time_minutes = int(start_time_minutes)
 
 
 
