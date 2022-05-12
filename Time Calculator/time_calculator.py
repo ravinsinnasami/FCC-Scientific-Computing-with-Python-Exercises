50795db51ada155015bd8ab8e6780d6a089190ad
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-12 19:23:30 +0800

    [EDIT] Update time_calculator.py
    - Added variables for time_denomination, change_counter, day_changed
      and no_od_days_passed
    - Added a for loop that adds the duration hour to the start time. Loop
      also changes to 0 every time it reaches to 12 except if 12 is the last
      value.
      * Loop also has a counter that add every time the start time reaches
        12.

--- Time Calculator/time_calculator.py
+++ Time Calculator/time_calculator.py
@@ -14,6 +14,32 @@ def add_time(start, duration, day=''):
     start_time_hour = int(start_time_hour)
     start_time_minutes = int(start_time_minutes)
 
+    # added time_denomination for comparison later.
+    # added counter that check how many times the hours go past 12 [both AM and PM]
+    # set initial day_changed = False. This is used to check if the time shifts from PM to AM
+    # no_of_days keeps count of how many times it turns from PM to AM
+    time_denomination = ['AM', 'PM']
+    denomination_change_counter = 0
+    day_changed = False
+    no_of_days_passed = 0
+
+    # variable that is to be returned
+    new_time = ''
+
+    # loop through the duration hour. Add 1 to start hour. If reaches 12, check if its the last value, if yes keep as 12
+    # else, change hour to 0 and start again.
+    # denomination counter +1 everything it reaches to 12.
+    for hour in range(duration_hour):
+        start_time_hour += 1
+
+        if start_time_hour == 12:
+            if hour == (duration_hour - 1):
+                start_time_hour = 12
+            else:
+                start_time_hour = 0
+
+            denomination_change_counter += 1
+
 
 
 
