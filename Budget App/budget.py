70edd4d31d0a98702c146b13b3edf4a37c92bda9
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-14 01:20:52 +0800

    [EDIT] Updated budget.py
    - initialized the Class
    - Added the deposit method
    - added variables for deposit method in initialization.

--- Budget App/budget.py
+++ Budget App/budget.py
@@ -1,6 +1,23 @@
-class Category:
+class Category():
 
+    # initialize Category class
 
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+        # variables for methods. instance variables.
+
+        # variables for def deposit
+        self.deposit_amount = 0
+        self.deposit_description = ''
+
+    # a method to deposit.
+    # requires amount, but description is optional
+    # appends the amount and description as dictionary into ledger list.
+    def deposit(self, amount, description=''):
+        self.deposit_amount = amount
+        self.deposit_description = description
+        self.ledger.append({'amount': self.deposit_amount, 'description': self.deposit_description})
 
 
 
