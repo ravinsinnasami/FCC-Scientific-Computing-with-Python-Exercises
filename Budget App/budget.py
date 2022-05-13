74fa48dacb0d9301bf73464720ee6c526eed288a
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-14 01:35:59 +0800

    [EDIT] Update budget.py
    - Added method for withdraw
    - Added method for getting balance
    - Added variables for withdraw and balance in the initialization.

--- Budget App/budget.py
+++ Budget App/budget.py
@@ -11,6 +11,13 @@ class Category():
         self.deposit_amount = 0
         self.deposit_description = ''
 
+        # variable for def withdraw
+        self.withdraw_amount = 0
+        self.withdraw_description = ''
+
+        # variable for def get_balance
+        self.balance = 0
+
     # a method to deposit.
     # requires amount, but description is optional
     # appends the amount and description as dictionary into ledger list.
@@ -18,6 +25,27 @@ class Category():
         self.deposit_amount = amount
         self.deposit_description = description
         self.ledger.append({'amount': self.deposit_amount, 'description': self.deposit_description})
+
+    # method to withdraw
+    # requires amount but description is optional.
+    # check if deposit amount is more than withdrawal amount, if yes, append to ledger and returns True. Else, return False.
+    def withdraw(self, amount, description=''):
+        self.withdraw_amount = amount
+        self.withdraw_description = description
+        if self.deposit_amount > self.withdraw_amount:
+            self.ledger.append({'amount': -abs(self.withdraw_amount), 'description': self.withdraw_description})
+            return True
+        else:
+            return False
+
+    # method to get balance
+    # loop through every element in ledger list. Now the element is a dictionary. So the amount value is accessed used the key.
+    # each time the value is obtained from ledger, it is added to the balance variable.
+    # lastly, return balance variable.
+    def get_balance(self):
+        for element in self.ledger:
+            self.balance += element['amount']
+        return self.balance
 
 
 
