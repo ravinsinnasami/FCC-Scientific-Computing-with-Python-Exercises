a66b263873ec3d14e42317c5b34a9adc140384e6
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-16 05:58:38 +0800

    [EDIT] Update shape_calculator.py
    - initialized Square class as subclass
    - Added code section to inherit all attribute and methods from parent class
    - Added method to set a new side value [to both width and height]
    - Added method to set a new width value [sets both width and height]
    - Added method to set a new height value [sets both width and height]
    - Added method to print string when the object instance is printed.

--- Polygon Area Calculator/shape_calculator.py
+++ Polygon Area Calculator/shape_calculator.py
@@ -54,5 +54,31 @@ class Rectangle:
         return f"Rectangle(width={self.width}, height={self.height})"
 
 
-class Square:
-    pass
+class Square(Rectangle):
+    """ Creates a square object """
+
+    def __init__(self, side):
+        """ Initialize the square object. Inherits all from the Parent class plus additional attribute (side)
+            - side parameter is passed as argument to Rectangle class. """
+        super().__init__(side, side)  # assigns to side value to width and length of Rectangle
+
+    def set_side(self, new_side):
+        """ Method to set a new side for the square
+            - additionally new side also sets the height and width """
+        self.width = new_side
+        self.height = new_side
+
+    def set_width(self, new_width):
+        """ Method to set a new width. New width is also set to the height. """
+        self.width = new_width
+        self.height = new_width
+
+    def set_height(self, new_height):
+        """ Method to set a new height. New height is also set to the width """
+        self.height = new_height
+        self.width = new_height
+
+    def __repr__(self):
+        """ Method to print out current sides of rectangle.
+            It doesn't matter if width or height is used to represent the side length """
+        return f"Square(side={self.width})"
