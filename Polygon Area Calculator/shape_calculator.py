ad9375d1e4ce55b15e0d6ab80fdc75f4eb5ceb3f
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-16 05:39:17 +0800

    [EDIT]  Update shape_calculator.py
    - Added code that calculates the diagonal and returns it
    - Added code that prints the shape based on width and height.[uses loop]
    - Added code that calculates how many other shape can fit within the
      current shape.
    - Added code to print a string when the object instance is printed.

--- Polygon Area Calculator/shape_calculator.py
+++ Polygon Area Calculator/shape_calculator.py
@@ -24,6 +24,34 @@ class Rectangle:
         perimeter = (2 * self.width) + (2 * self.height)
         return perimeter
 
+    def get_diagonal(self):
+        """ Method that calculates the diagonal of a rectangle and returns it
+            - no math library or pow for calculation; only arithmetics symbols """
+        diagonal = ((self.width ** 2) + (self.height ** 2)) ** 0.5
+        return diagonal
+
+    def get_picture(self):
+        """ Method that prints the shape of the rectangle using set height and width """
+        picture = ''
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:
+            for height in range(self.height):
+                for width in range(self.width):
+                    picture += '*'
+                picture += "\n"
+            return picture
+
+    def get_amount_inside(self, inside_shape):
+        """ Method to calculate how many of a certain shape can fit in current shape """
+        current_shape_area = self.get_area()
+        inside_shape_area = inside_shape.get_area()
+        total_fit = current_shape_area // inside_shape_area # Forces to a whole number
+        return total_fit
+
+    def __repr__(self):
+        """ Method to print out current width and length of Rectangle """
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 
 class Square:
