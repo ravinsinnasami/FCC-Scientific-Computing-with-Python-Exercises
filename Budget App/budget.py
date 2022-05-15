06e84bc73e2cc2efbfc3c807a9dcbfc16b3bd702
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-15 23:26:02 +0800

    [EDIT] update budget.py
    - edited the comments before each class methods into docstring within
      the methods
    - also edited the first comment under the outside class into a docstring

--- Budget App/budget.py
+++ Budget App/budget.py
@@ -1,11 +1,18 @@
 class Category():
 
-    # initialize Category class
+    """Creates a Category Class"""
+
     def __init__(self, category):
+        """ Initializes the variables that are necessary for the class and methods.
+             - first initialization is for category naming.
+             - second initialization is for creation of ledger list
+             - rest is variables for methods.
+            """
+
         self.category = category
         self.ledger = []
-        # variables for methods. instance variables.
 
+        # variables for methods. instance variables.
         # variables for def deposit
         self.deposit_amount = 0
         self.deposit_description = ''
@@ -24,18 +31,23 @@ class Category():
         self.check_funds_amount = 0
 
 
-    # a method to deposit.
-    # requires amount, but description is optional
-    # appends the amount and description as dictionary into ledger list.
     def deposit(self, amount, description=''):
+        """ Method for adding in deposit.
+            Amount is required argument but description is optional.
+            Both amount and description is appended into the list as a dictionary
+            """
+
         self.deposit_amount = amount
         self.deposit_description = description
         self.ledger.append({'amount': self.deposit_amount, 'description': self.deposit_description})
 
-    # method to withdraw
-    # requires amount but description is optional.
-    # check if deposit amount is more than withdrawal amount, if yes, append to ledger and returns True. Else, return False.
+
     def withdraw(self, amount, description=''):
+        """  Method for withdrawing funds.
+             Amount is required argument but description is optional.
+             Check if deposit amount is equal to or larger than withdrawal. Return True or false appropriately.
+             """
+
         self.withdraw_amount = amount
         self.withdraw_description = description
         if self.deposit_amount > self.withdraw_amount:
@@ -44,24 +56,31 @@ class Category():
         else:
             return False
 
-    # method to get balance
-    # loop through every element in ledger list. Now the element is a dictionary. So the amount value is accessed used the key.
-    # each time the value is obtained from ledger, it is added to the balance variable.
-    # lastly, return balance variable.
+
     def get_balance(self):
+        """ Method to get balance based on items on the ledger list.
+            Sets balance = 0, so that each time it loops through the list it starts at 0.
+                - self.balance is not set in def __init__ because if set there, last final value will be used as initial value when checking balance of
+                list.
+            Returns the final balance value.
+            """
+
         self.balance = 0
         for element in self.ledger:
             self.balance += element['amount']
         self.current_balance = self.balance
         return self.current_balance
 
-    # method to transfer funds.
-    # transfer_category_obj is another instance
-    # check if there is enough deposit to do transfer. if unable, return False. Else:
-        # send info to withdraw method
-        # append info to transfer_category_obj ledger
-        # return True
+
     def transfer(self, amount, transfer_category_obj):
+        """ Method to transfer funds to another category.
+            transfer_category_obj is a class instance. It'll be treated as such without any self.[]
+            Check if transfer amount is equal or lower than deposit value. If no, return False. Else:
+                - sends info to withdraw method - to remove fund
+                - appends into to ledger of the transfer_category_obj
+                - returns True
+            """
+
         self.transfer_amount = amount
         if self.deposit_amount > self.transfer_amount:
             self.withdraw(self.transfer_amount, f"Transfer to {transfer_category_obj.category}")
@@ -70,19 +89,30 @@ class Category():
         else:
             return False
 
-    # method to check balance
-    # get balance from def get_balance and compare against given value.
-    # if balance is more or equal to amount, return True. Else, return False.
+
     def check_funds(self, amount):
+        """ Method to check balance and compare with given value.
+            uses get_balance method to get a value and then compares with given value.
+            If balance is more than given value, return True, else return False.
+            """
+
         self.check_funds_amount = amount
         if self.get_balance() >= self.check_funds_amount:
             return True
         else:
             return False
 
-    # method to print the instance
-    # each print is appended or concatenated to the final_print variable which is then returned.
+.
     def __repr__(self):
+        """ Method to print a set things when the object is printed.
+            creates a variable to store the strings.
+                - adds a string containing category, centered
+                - loop through the ledger list and prints the description (first 23 chars, left adjusted)
+                    and then the amount (right adjust, max 7 characters, 2 decimal points)
+                - adds a string with the final total
+            returns the final variable
+            """
+
         final_prints = ''
         final_prints += f"{self.category.title().center(30, '*')}\n"
         for element in self.ledger:
@@ -93,7 +123,7 @@ class Category():
 
 
 def create_spend_chart(categories):
-    # Create a chart that maps the percentage of expenses spent based on total withdrawal
+    """ Create a chart that maps the percentage of expenses spent based on total withdrawal"""
 
     # get the total expense spent [sum of all the categories]
     total_expense = 0
