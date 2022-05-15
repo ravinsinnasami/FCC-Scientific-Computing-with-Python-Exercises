028012b230b0c24bc98a0f826002483f593c76bd
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-16 05:24:42 +0800

    [EDIT] Update shape_calculator.py
    - added code for initialization
    - added code for setting a new width
    - added code for setting a new height
    - added code for calculating area and returning it
    - added code for calculating perimeter and returning it

--- Polygon Area Calculator/shape_calculator.py
+++ Polygon Area Calculator/shape_calculator.py
@@ -1,5 +1,28 @@
 class Rectangle:
-    pass
+    """ creates a rectangle object """
+
+    def __init__(self, width, height):
+        """ Initializes the rectangle class """
+        self.width = width
+        self.height = height
+
+    def set_width(self, new_width):
+        """ Method to set a new width """
+        self.width = new_width
+
+    def set_height(self, new_height):
+        """ Method to set a new height """
+        self.height = new_height
+
+    def get_area(self):
+        """ Method that calculates the area of the rectangle and returns it """
+        area = self.width * self.height
+        return area
+
+    def get_perimeter(self):
+        """ Method that calculates the perimeter of the rectangle and returns it """
+        perimeter = (2 * self.width) + (2 * self.height)
+        return perimeter
 
 
 
