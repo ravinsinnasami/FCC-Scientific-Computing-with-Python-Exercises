6b11ca7c0ff603ede70dc2b18e6ffeba6f093294
ravinsinnasami <ravinsinnasami@gmail.com>
2022-05-17 22:40:02 +0800

    [EDIT] Update prob_calculator.py
    - Added initialization for the Hat Class
    - Added method to append the content of the dictionary to a list.

--- Probability Calculator/prob_calculator.py
+++ Probability Calculator/prob_calculator.py
@@ -3,7 +3,20 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-    pass
+
+    """ Creates a Hat Class """
+
+    def __init__(self, **balls):
+        """ Initializes the required variables and lists """
+        self.balls_dictionary = balls
+        self.contents = []
+        self.set_contents()
+
+    def set_contents(self):
+        """ Method to appends the color of the balls to a list based on the value of the color in the dictionary """
+        for key in self.balls_dictionary:
+            for value in range(self.balls_dictionary[key]):
+                self.contents.append(key)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
